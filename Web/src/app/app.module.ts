11cd9be74dc216b0ed9d27eb564381e41fd7332d
JACKQUELINE REYES ALVARADO <jackqueline_93@hotmail.com>
2018-10-28 17:47:00 -0500

    Build Home Component App

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCheckboxModule, MatSidenavModule, MatIconModule} from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterAccountComponent } from './register-account/register-account.component';
+
+const appRoutes: Routes=[
+  { path: 'home', component: HomeComponent },
+  { path: 'register-account', component: RegisterAccountComponent },
+  { path: 'login', component: LoginComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterAccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatIconModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
